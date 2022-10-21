56dc473edfa92dba319126b17810ceab5134b9fc
Zack <schaxmann@gmail.com>
2022-10-21 11:59:55 +0100

    Adds iphone resolution box to homepage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,50 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Box, ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "reset-css";
+// import PlayerLayout from "../components/PlayerLayout";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#F5F5F5",
+      200: "#EEEEEEE",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      variants: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
+
+// { Component, pageProps }
+
+function MyApp() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box height="100vh" width="calc(100vh * 0.46208530805)" bg="black"></Box>
+      {/* {Component.authpage ? (
+        <Component {...pageProps} />
+      ) : (
+        <PlayerLayout>
+          <Component {...pageProps} />
+        </PlayerLayout>
+      )} */}
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
