3867f236050b09f093b62f9ccfda14ffeed4d95a
Zack <schaxmann@gmail.com>
2022-10-21 13:47:54 +0100

    Improves coloumn text spacing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
-import { Box, ChakraProvider, extendTheme, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  calc,
+  ChakraProvider,
+  extendTheme,
+  Flex,
+  Text,
+} from "@chakra-ui/react";
 import "reset-css";
+import NextImage from "next/image";
 
 const theme = extendTheme({
   colors: {
@@ -34,9 +42,34 @@ const theme = extendTheme({
 function MyApp() {
   return (
     <ChakraProvider theme={theme}>
-      <Flex position="absolute" height="100vh" width="100vw" justify="center">
-        <Box id="leftColumn" height="100vh" flexGrow="1" bg="gray.400">
-          <p>I'm the left column</p>
+      <Flex
+        position="absolute"
+        height="100vh"
+        width="100vw"
+        justify="center"
+        justifyContent="space-between"
+      >
+        <Box
+          id="leftColumn"
+          height="100vh"
+          maxWidth="calc(50vw - 50vh * 9 / 19.5)"
+          flexGrow="1"
+          bg="gray.400"
+        >
+          <Box height="100%" p="50px" pt="0">
+            <NextImage src="/favicon" width="20px" height="20px"></NextImage>
+            <Text fontSize="5xl">Natter.ly</Text>
+            <Text fontSize="3xl">The home of interesting conversation</Text>
+            <Text fontSize="2xl">
+              Natterly was founded on the fundamental belief that good
+              conversation is food for the soul.
+            </Text>
+            <Text fontSize="2xl">
+              The framework we have created exists to facilitate meaningful
+              one-to-one interaction at scale, for those tired of traditional
+              platforms.
+            </Text>
+          </Box>
         </Box>
         <Box
           id="boundingBox"
@@ -45,8 +78,22 @@ function MyApp() {
           minWidth="calc(100vh * (9/19.5))"
           bg="black"
         ></Box>
-        <Box id="rightColumn" height="100vh" bg="gray.400" flexGrow="1">
-          <p>I'm the right column</p>
+        <Box
+          id="leftColumn"
+          height="100vh"
+          maxWidth="calc(50vw - 50vh * 9 / 19.5)"
+          flexGrow="1"
+          bg="gray.400"
+        >
+          <Box height="100%" p="50px" pt="0">
+            <Text fontSize="3xl" textAlign="right">
+              Under The Hood:
+            </Text>
+            <Text fontSize="2xl" textAlign="right">
+              Natterly was founded on the fundamental belief that good
+              conversation is food for the soul.
+            </Text>
+          </Box>
         </Box>
       </Flex>
     </ChakraProvider>
