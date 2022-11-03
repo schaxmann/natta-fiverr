935d81dd53000bdaea817cb36e647032efe63ecc
Zack <schaxmann@gmail.com>
2022-11-03 23:15:08 +0000

    Adds switch for dark/light mode

--- components/RightColumn.tsx
+++ components/RightColumn.tsx
@@ -1,16 +1,27 @@
-import { Box, Heading, Text, Flex } from "@chakra-ui/react";
+import { Box, Heading, Text, Flex, Switch } from "@chakra-ui/react";
 
 const RightColumn = () => {
   return (
     <Flex
       id="rightColumn"
       height="100vh"
+      flexDir="column"
       maxWidth="calc(50vw - 50vh * 9 / 19.5)"
       flexGrow="1"
       bg="white"
       alignItems="center"
     >
-      <Box pr="3.15rem" pl="7rem">
+      <Flex flexGrow="1" height="calc(100vh / 3)">
+        <Switch p="8"></Switch>
+      </Flex>
+      <Flex
+        flexDir="column"
+        flexGrow="1"
+        pr="3.15rem"
+        pl="7rem"
+        height="calc(100vh / 3)"
+        justifyContent="center"
+      >
         <Heading fontSize="3xl" textAlign="right" fontWeight="800">
           Under The Hood:
         </Heading>
@@ -18,7 +29,10 @@ const RightColumn = () => {
           This section will contain explanations of features and functionality
           during demonstration of the app.
         </Text>
-      </Box>
+      </Flex>
+      <Flex flexGrow="1" height="calc(100vh / 3)">
+        <Box></Box>
+      </Flex>
     </Flex>
   );
 };
